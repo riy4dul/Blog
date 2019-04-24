06739dab17289aecfd6143b18a7d28b0e37095d1
Riyadul <starriyadul@gmail.com>
2019-04-25 01:16:11 +0600

    Dashboard clean

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -68,73 +68,7 @@
                                 </div>
                             </div>
                             <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header card-header-rose card-header-icon">
-                        <h3 style="color: red" class="card-title text-center"><b>Pending Post</b></h3>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Photo</th>
-                                        <th>Title</th>
-                                        <th>Status</th>
-                                        <th>Time</th>
-                                        <th class="disabled-sorting text-right">Actions</th>
-                                    </tr>
-                                </thead>
-                                {{--<tbody>--}}
-                                    {{--@foreach($reservations as $key=>$reservation)--}}
-                                    {{--<tr>--}}
-                                        {{--<td style="width:3%">{{$key + 1}}</td>--}}
-                                        {{--<td style="width:10%">{{$reservation->name}}</td>--}}
-                                        {{--<td style="width:10%">{{$reservation->phone}}</td>--}}
-                                        {{--<td style="width:15%">{{$reservation->email}}</td>--}}
-                                        {{--<td style="width:15%">{{$reservation->date_and_time}}</td>--}}
-                                        {{--<td style="width:15%">{{$reservation->message}}</td>--}}
-                                        {{--<td style="width:15%">--}}
-                                            {{--@if($reservation->status == true)--}}
-                                                {{--<button class="btn btn-success btn-sm">Confirmed</button>--}}
-                                            {{--@else--}}
-                                                {{--<button class="btn btn-danger btn-sm">Not Confirmed</button>--}}
-                                            {{--@endif--}}
-                                        {{--</td>--}}
-                                        {{--<td style="width:10%">{{$reservation->created_at}}</td>--}}
-                                        {{--<td>--}}
-                                                {{--@if($reservation->status == false)--}}
-                                                    {{--<form id="status-form-{{ $reservation->id }}" action="{{ route('reservation.status',$reservation->id) }}" style="display: none;" method="POST">--}}
-                                                        {{--@csrf--}}
-                                                    {{--</form>--}}
-                                                    {{--<button type="button" class="btn btn-info btn-sm" onclick="if(confirm('Are you verify this request by phone?')){--}}
-                                                            {{--event.preventDefault();--}}
-                                                            {{--document.getElementById('status-form-{{ $reservation->id }}').submit();--}}
-                                                            {{--}else {--}}
-                                                            {{--event.preventDefault();--}}
-                                                            {{--}"><i class="material-icons">done</i></button>--}}
-                                                {{--@endif--}}
-                                                {{--<form id="delete-form-{{ $reservation->id }}" action="{{ route('reservation.destory',$reservation->id) }}" style="display: none;" method="POST">--}}
-                                                    {{--@csrf--}}
-                                                    {{--@method('DELETE')--}}
-                                                {{--</form>--}}
-                                                {{--<button type="button" class="btn btn-danger btn-sm" onclick="if(confirm('Are you sure? You want to delete this?')){--}}
-                                                    {{--event.preventDefault();--}}
-                                                    {{--document.getElementById('delete-form-{{ $reservation->id }}').submit();--}}
-                                                {{--}else {--}}
-                                                    {{--event.preventDefault();--}}
-                                                        {{--}"><i class="material-icons">delete</i></button>--}}
-                                        {{--</td>--}}
-                                    {{--</tr>--}}
-                                    {{--@endforeach--}}
-                                {{--</tbody>--}}
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                </div> <!-- end col-md-12 -->
-                </div> <!-- end row -->
+
                             {{-- <h3>Manage Listings</h3>
                             <br>
                             <div class="row">
